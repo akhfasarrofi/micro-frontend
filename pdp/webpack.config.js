c94b2011c12f3c07f5fd1441f76320139f623eb1
akhfas <zakaria@aksaramaya.com>
2022-05-29 20:28:59 +0700

    change port and add react router dom

--- pdp/webpack.config.js
+++ pdp/webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const deps = require('./package.json').dependencies;
 module.exports = {
   output: {
-    publicPath: 'http://localhost:30001/',
+    publicPath: 'http://localhost:3001/',
   },
 
   resolve: {
@@ -12,7 +12,7 @@ module.exports = {
   },
 
   devServer: {
-    port: 30001,
+    port: 3001,
     historyApiFallback: true,
   },
 
